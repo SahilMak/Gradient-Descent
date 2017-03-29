e2f9f7c89a837a9a748e879a7ceff170ebf3ed43
Sahil Makhijani <svmakhijani94@gmail.com>
2017-03-29 19:09:11 -0400

    Added loop for batch calculation

--- batch.m
+++ batch.m
@@ -10,5 +10,10 @@ old1 = w1;
 old2 = w2;
 count = 0;
 while count < 1000000
+    new1 = old1 + a * summation();
+    new2 = old2 + a * summation();
+    old1 = new1;
+    old2 = new2;
+    count = count + 1;
 end
 end
