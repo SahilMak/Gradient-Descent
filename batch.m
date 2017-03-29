f5a9fdb0c3513427ad262a25d25414e405684fdf
Sahil Makhijani <svmakhijani94@gmail.com>
2017-03-29 19:15:03 -0400

    Added break conditions for batch calculation

--- batch.m
+++ batch.m
@@ -15,5 +15,13 @@ while count < 1000000
     old1 = new1;
     old2 = new2;
     count = count + 1;
+    if (abs(w1 - new1) < 10^-10) && (abs(w2 - new2) < 10^-10)
+        fprintf('Weight 1: %f\nWeight 2: %f\n', new1, new2);
+        break;
+    end
+    if isinf(new1) || isinf(new2) || isnan(new1) || isnan(new2)
+        fprintf('Breaks at epoch: %d\n', count);
+        break;
+    end
 end
 end
