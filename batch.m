40742502b229d54b79c2cc0fc999b91490e016b0
Sahil Makhijani <svmakhijani94@gmail.com>
2017-03-29 19:01:26 -0400

    Set up framework for batch function

---
+++ batch.m
@@ -0,0 +1,14 @@
+function batch(w1, w2, a, data)
+%Batch calculates batch gradient descent
+% Inputs:
+%  w1 = 1st weight value
+%  w2 = 2nd weight value
+%  a = learning rate value
+%  data = numeric dataset
+
+old1 = w1;
+old2 = w2;
+count = 0;
+while count < 1000000
+end
+end
