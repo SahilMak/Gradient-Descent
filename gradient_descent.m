153e75505525b7b7a03dd2d64a24be6295834edf
Sahil Makhijani <svmakhijani94@gmail.com>
2017-03-29 18:53:49 -0400

    Configured user input for dataset

--- gradient_descent.m
+++ gradient_descent.m
@@ -1,4 +1,15 @@
 clear;
+rows = input('How many rows does the dataset have?: ');
+dataset = zeros(rows,2);
+for i = 1:rows
+    for j = 1:2
+        if j == 1
+            dataset(i,j) = input(['Enter the x-value for row ',num2str(i),': ']);
+        else
+            dataset(i,j) = input(['Enter the y-value for row ',num2str(i),': ']);
+        end
+    end
+end
 % dataset = user input
 % w1 = user input
 % w2 = user input
